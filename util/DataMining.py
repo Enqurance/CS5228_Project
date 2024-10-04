ae24980bbb42f3088fb03072c39e39b19f923093
Enqurance <20373980@buaa.edu.cn>
2024-10-04 21:28:03 +0800

    EDA and data preprocess

--- util/DataMining.py
+++ util/DataMining.py
@@ -4,7 +4,49 @@ from sklearn.svm import SVR
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
+
 import numpy as np
+import pandas as pd
+
+
+def RandomForestMiningByModel(x_train, x_test, y_train, y_test):
+	# Normalize the attributes
+	model_dict = {}
+	y_pred = None
+	for model_type, group in x_train.groupby('model'):
+		X = group.drop(['model'], axis=1)
+		y = y_train[y_train['model'] == model_type].drop(['model'], axis=1)
+
+		model = RandomForestRegressor(n_estimators=100, random_state=42)
+
+		model.fit(X, np.ravel(y))
+		model_dict[model_type] = model
+
+	for model_type, group in x_test.groupby('model'):
+		X = group.drop(['model'], axis=1)
+
+		if model_type not in model_dict.keys():
+			y_test = y_test[y_test['model'] != model_type]
+			continue
+
+		model = model_dict[model_type]
+		y_pred_new = model.predict(X)
+
+		# 将预测结果与索引结合为 DataFrame
+		y_pred_new_df = pd.DataFrame(y_pred_new, index=X.index, columns=['prediction'])
+
+		# 将新的预测结果与之前的结果合并
+		if y_pred is None:
+			y_pred = y_pred_new_df
+		else:
+			y_pred = pd.concat([y_pred, y_pred_new_df])
+
+	y_test = y_test.drop(['model'], axis=1)
+	y_test_aligned = y_test.reindex(y_pred.index)
+	rmse = np.sqrt(mean_squared_error(y_test_aligned, y_pred))
+	print(f'RMSE on test data: {rmse}')
+
+	return rmse
 
 
 def RandomForestMining(x_train, x_test, y_train, y_test):
@@ -12,7 +54,6 @@ def RandomForestMining(x_train, x_test, y_train, y_test):
 	scaler = StandardScaler()
 	x_train = scaler.fit_transform(x_train)
 	x_test = scaler.transform(x_test)
-
 	model = RandomForestRegressor(n_estimators=100, random_state=42)
 	model.fit(x_train, y_train)
 	y_pred = model.predict(x_test)
@@ -61,9 +102,17 @@ def KNNMining(x_train, x_test, y_train, y_test):
 	print(f'Root Mean Squared Error on Test Set: {rmse}')
 
 
-def split_dataframe(df, target_column, test_size=0.2, random_state=None):
-	X = df.drop(columns=[target_column])
-	y = df[target_column]
+def split_dataframe(df, target_cols, test_size=0.2, random_state=None):
+	X = df.drop(columns=target_cols)
+	y = df[target_cols]
+	x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+	return x_train, x_test, y_train, y_test
+
+
+def split_dataframe_flex(df, train_drop_cols, test_cols, test_size=0.2, random_state=None):
+	X = df.drop(columns=train_drop_cols)
+	y = df[test_cols]
 	x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
 	return x_train, x_test, y_train, y_test
