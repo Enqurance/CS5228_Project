9b8252e33f08dcbd0bf955a8d30d54411bedf06a
Enqurance <20373980@buaa.edu.cn>
2024-10-12 16:39:45 +0800

    Test random forest & linear conbination method

--- util/DataMining.py
+++ util/DataMining.py
@@ -1,15 +1,97 @@
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.svm import SVR
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import GradientBoostingClassifier
 
 import numpy as np
 import pandas as pd
 
 
-def RandomForestMiningByModel(x_train, x_test, y_train, y_test):
+def CombinedDataMiningRandomForestAndLinearRegression(df, df_test):
+	df_dereg_nan = df_test[df_test['dereg_value'].isna()].reset_index(drop=False)
+	df_dereg = df_test[df_test['dereg_value'].notna()].reset_index(drop=False)
+	# For rows with dereg_value, we apply linear regression here
+	x_train = df[['dereg_value', 'model']]
+	y_train = df[['price', 'model']]
+	x_test = df_dereg[['dereg_value', 'model']]
+	pred_dereg = LinearRegressionMiningByModel(x_train, x_test, y_train, dev=True)
+
+	# For rows without dereg_value, we apply random forest here
+	x_train = df.drop(columns=['price', 'dereg_value', 'depreciation'])
+	y_train = df['price']
+	x_test = df_dereg_nan.drop(columns=['dereg_value', 'depreciation', 'index'])
+	pred_dereg_nan = RandomForestMining(x_train, x_test, y_train, dev=True)
+	pred_dereg['index'] = df_dereg['index'].values
+	pred_dereg_nan['index'] = df_dereg_nan['index'].values
+
+	combined_pred = pd.concat([pred_dereg, pred_dereg_nan])
+	combined_pred = combined_pred.sort_values(by='index').reset_index(drop=True)
+	combined_pred['Id'] = range(len(combined_pred))
+	combined_pred = combined_pred.drop(columns=['index'])
+
+	return combined_pred
+
+
+def LinearRegressionMiningByModel(x_train, x_test, y_train, y_test=None, dev=False):
+	# Normalize the attributes
+	model_dict = {}
+	y_pred = None
+	for model_type, group in x_train.groupby('model'):
+		X = group.drop(['model'], axis=1)
+		y = y_train[y_train['model'] == model_type].drop(['model'], axis=1)
+
+		model = LinearRegression()
+		model.fit(X, y)
+		model_dict[model_type] = model
+
+	for model_type, group in x_test.groupby('model'):
+		X = group.drop(['model'], axis=1)
+		model = model_dict[model_type]
+		y_pred_new = model.predict(X)
+
+		y_pred_new_df = pd.DataFrame(y_pred_new, index=X.index, columns=['Predicted'])
+
+		if y_pred is None:
+			y_pred = y_pred_new_df
+		else:
+			y_pred = pd.concat([y_pred, y_pred_new_df])
+
+	y_pred_aligned = y_pred.loc[x_test.index]
+	if dev:
+		return y_pred_aligned
+	else:
+		y_test = y_test.drop(['model'], axis=1)
+		y_test_aligned = y_test.reindex(y_pred_aligned.index)
+		print('Running not in develop mode')
+		rmse = np.sqrt(mean_squared_error(y_test_aligned, y_test))
+		print(f'RMSE on test data: {rmse}')
+		return rmse
+
+
+def LinearRegressionMining(x_train, x_test, y_train, y_test=None, dev=False):
+	model = LinearRegression()
+	model.fit(x_train, y_train)
+
+	y_pred = model.predict(x_test)
+	# Calculate RMSE
+	if dev:
+		ids = [i for i in range(len(x_test))]
+		return pd.DataFrame(
+			list(zip(ids, y_pred)),
+			columns=['Id', 'Predicted']
+		)
+	else:
+		print('Running not in develop mode')
+		rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+		print(f'RMSE on test data: {rmse}')
+		return rmse
+
+
+def RandomForestMiningByModel(x_train, x_test, y_train, y_test=None, dev=False):
 	# Normalize the attributes
 	model_dict = {}
 	y_pred = None
@@ -32,10 +114,8 @@ def RandomForestMiningByModel(x_train, x_test, y_train, y_test):
 		model = model_dict[model_type]
 		y_pred_new = model.predict(X)
 
-		# 将预测结果与索引结合为 DataFrame
 		y_pred_new_df = pd.DataFrame(y_pred_new, index=X.index, columns=['prediction'])
 
-		# 将新的预测结果与之前的结果合并
 		if y_pred is None:
 			y_pred = y_pred_new_df
 		else:
@@ -43,10 +123,36 @@ def RandomForestMiningByModel(x_train, x_test, y_train, y_test):
 
 	y_test = y_test.drop(['model'], axis=1)
 	y_test_aligned = y_test.reindex(y_pred.index)
-	rmse = np.sqrt(mean_squared_error(y_test_aligned, y_pred))
-	print(f'RMSE on test data: {rmse}')
+	if dev:
+		ids = [i for i in range(len(x_test))]
+		return pd.DataFrame(
+			list(zip(ids, y_pred)),
+			columns=['Id', 'Predicted']
+		)
+	else:
+		print('Running not in develop mode')
+		rmse = np.sqrt(mean_squared_error(y_test_aligned, y_pred))
+		print(f'RMSE on test data: {rmse}')
+		return rmse
 
-	return rmse
+
+def GradientBoostingMining(x_train, x_test, y_train, y_test=None, dev=False):
+	model = GradientBoostingClassifier(n_estimators=20, learning_rate=0.01, max_depth=3, random_state=42)
+	model.fit(x_train, y_train)
+
+	y_pred = model.predict(x_test)
+	# Calculate RMSE
+	if dev:
+		ids = [i for i in range(len(x_test))]
+		return pd.DataFrame(
+			list(zip(ids, y_pred)),
+			columns=['Id', 'Predicted']
+		)
+	else:
+		print('Running not in develop mode')
+		rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+		print(f'RMSE on test data: {rmse}')
+		return rmse
 
 
 def RandomForestMining(x_train, x_test, y_train, y_test=None, dev=False):
@@ -55,7 +161,7 @@ def RandomForestMining(x_train, x_test, y_train, y_test=None, dev=False):
 	x_train = scaler.fit_transform(x_train)
 	x_test = scaler.transform(x_test)
 	model = RandomForestRegressor(
-		n_estimators=300,
+		n_estimators=500,
 		max_depth=16
 	)
 	model.fit(x_train, y_train)
